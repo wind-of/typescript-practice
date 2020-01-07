78b5f964c00892f6fcf2bb0da8672854f8dfb353
wind-of <adequation@mail.ru>
2020-01-07 11:32:59 +0300

    additional

--- index.ts
+++ index.ts
@@ -39,3 +39,13 @@ anyType = true; // boolean
 anyType = 15; // number
 anyType = null; /// null
 // etc.
+
+
+
+
+//                              ***Destructurization*** 
+enum Something {first, second, third};
+const {first, second, third}: {first: number, second: number, third: number} = {...Something}
+const [smth, anothersmth]: string[] = ['string', 'string'];
+const [tupleSmth, tupleAnother]: [string, number] = ['string', 61];
+// etc\
