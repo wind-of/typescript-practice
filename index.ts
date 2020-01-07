4d13d02704978441e62539e2e4d733f0b12a76a0
wind-of <adequation@mail.ru>
2020-01-07 11:32:00 +0300

    Enum

--- index.ts
+++ index.ts
@@ -13,3 +13,18 @@ const tupleType: [string, number, boolean, boolean, number] = ['string', 15, tru
   The order matters:
   [15, 'string',  true, false, 7] - throws an error
 */
+
+
+
+
+//                              ***Enum***
+enum Color {red, blue, green}
+const redColor: Color = Color.red; // 0
+const blueColor: Color = Color.blue; // 1
+const greenColor: Color = Color.green; // 2
+
+enum Numeration {one = 1, second, third} 
+
+enum List {first = 1, second = 'second'} // number or string or nothing
+const firstItem: List = List.first; // 1
+const secondItem: List = List.second; // 'second'
