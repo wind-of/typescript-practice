e0836230cdead3f77a99e0942789efca92c20cd3
wind-of <adequation@mail.ru>
2020-01-07 19:06:08 +0300

    Never

--- index.ts
+++ index.ts
@@ -73,7 +73,21 @@ function ReturnNullOrUndefined(): void {
 
 
 
+//                              ***Never***                              \\
+// "never" is the return type for a function expressions that always throws an exception or one that never returns
+function error(message: string): never {
+  throw new Error(message);
+}
 
+// The Inferred return type is never
+function fail() {
+  return error("Something failed");
+}
+
+// Function returning never must have unreachable end point
+function InfiniteLoopV2(): never {
+  while (true) {}
+}
 
 
 
