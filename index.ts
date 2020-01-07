b344d09c68ebe40018bf48b7ab251abc421a5039
wind-of <adequation@mail.ru>
2020-01-07 11:31:38 +0300

    Tuple types

--- index.ts
+++ index.ts
@@ -5,3 +5,11 @@ const object: object | Object = new Function() || [] || {} // etc.
 const Null: null = null;
 const Undefined: undefined = undefined;
 const symbol: Symbol = Symbol('Symbol');
+
+
+//                              ***Tuple***
+const tupleType: [string, number, boolean, boolean, number] = ['string', 15, true, false, 7];
+/* 
+  The order matters:
+  [15, 'string',  true, false, 7] - throws an error
+*/
