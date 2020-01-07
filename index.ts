036ac8d41edcb045974926c47fb3af11e8e2fa5a
wind-of <adequation@mail.ru>
2020-01-07 13:26:21 +0300

    more about enum

--- index.ts
+++ index.ts
@@ -23,11 +23,17 @@ const redColor: Color = Color.red; // 0
 const blueColor: Color = Color.blue; // 1
 const greenColor: Color = Color.green; // 2
 
-enum Numeration {one = 1, second, third} 
-
+// You can manually set the values in the enum 
 enum List {first = 1, second = 'second'} // number or string or nothing
 const firstItem: List = List.first; // 1
 const secondItem: List = List.second; // 'second'
+
+
+// You can also change the number at which start numbering of enum, like below:
+enum Numeration {one = 1, two, three}
+const one: Numeration = Numeration.one; // 1
+const two: Numeration = Numeration.two; // 2
+const three: Numeration = Numeration.three; // 3
 
 
 
