51dbaf514016b50631fb924b3d5c9fd22c76a553
wind-of <adequation@mail.ru>
2020-01-07 13:45:49 +0300

    Void

--- index.ts
+++ index.ts
@@ -47,6 +47,21 @@ anyType = null; /// null
 // etc.
 
 
+//                              ***Void*** 
+const VoudVariable: void = undefined || null;
+
+// If function doesn't return anything (except for null and undefined), it has a void type
+function InfinityLoop(): void {
+  while(true) {}
+}
+function OpenWindow(): void {
+  playground.IsWindowOpen = true;
+}
+function ReturnNullOrUndefined(): void {
+  return null || undefined
+  // ...or simply
+  return
+}
 
 
 //                              ***Destructurization*** 
