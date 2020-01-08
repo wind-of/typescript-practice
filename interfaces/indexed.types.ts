0a64c186ee67780d71416251b2f59179f91dd4f3
wind-of <adequation@mail.ru>
2020-01-08 17:13:49 +0300

    Indexed types

---
+++ interfaces/indexed.types.ts
@@ -0,0 +1,14 @@
+interface StringArray {
+  [index: number]: string;
+}
+
+const myArray: StringArray = ["Bob", "Fred"];
+
+
+
+
+interface NumberDictionary {
+  [index: string]: number;
+  length: number;    // ok, length is a number
+  // name: string;      // error, the type of 'name' is not a subtype of the indexer
+}
